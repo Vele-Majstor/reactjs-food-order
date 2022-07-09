9da91a3ef66e28ae94d0e77e8df13d914478fa54
Hristijan Veleski <hristijan1veleski@gmail.com>
2022-07-09 20:01:07 +0200

    merged confics

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <CartProvider>
-      {<Cart/>}
+      {cartIsShown &&<Cart onClose={hideCartHandler}/>}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
